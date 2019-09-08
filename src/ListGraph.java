0c3bcd8702af2c524a5c4669a9bbcb1a7aedfa1a
PKmnman <gareeves@my.bridgeport.edu>
2019-09-08 17:02:33 -0400

    Added "implements"

--- src/ListGraph.java
+++ src/ListGraph.java
@@ -1,15 +1,38 @@
+import com.graph.Graph;
+
 import java.util.ArrayList;
 
-public class ListGraph<T> {
-    private  ArrayList<T> vectors;
+public class ListGraph implements Graph<Integer, Integer> {
+    
+    private  ArrayList<GraphVector> vectors;
 
     public ListGraph(){
         this.vectors = null;
     }
 
-    public ListGraph(int x){
-        this.vectors = new ArrayList<>(x);
+    public ListGraph(int size){
+        this.vectors = new ArrayList<>(size);
     }
-
-
+    
+    @Override
+    public GraphVector<Integer, Integer> depthSearch(Integer nodeID) {
+        return null;
+    }
+    
+    @Override
+    public GraphVector<Integer, Integer> breadthSearch(Integer nodeID) {
+        return null;
+    }
+    
+    @Override
+    public void addVector(GraphVector<Integer, Integer> vector) {
+    
+    }
+    
+    @Override
+    public GraphVector<Integer, Integer>[] getVectorArray() {
+        return new Graph.GraphVector[0];
+    }
+    
+    private class ListVector extends Graph.GraphVector<Integer, Integer>
 }
