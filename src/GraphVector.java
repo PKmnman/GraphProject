c471695e57f31d9ccf0d8b1fb86780829f2963c9
Ricky He <rickyhe@my.bridgeport.edu>
2019-09-08 16:28:53 -0400

    Classes

---
+++ src/GraphVector.java
@@ -0,0 +1,14 @@
+public class GraphVector<T,V> {
+    private T identifier;
+    private V value;
+
+    public T getIdentifier(){
+        return this.identifier;
+    }
+
+    public V getValue(){
+        return this.value;
+    }
+
+
+}
---
+++ src/ListGraph.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class ListGraph<T> {
+    private  ArrayList<T> vectors;
+
+    public ListGraph(){
+        this.vectors = null;
+    }
+
+    public ListGraph(int x){
+        this.vectors = new ArrayList<>(x);
+    }
+
+
+}
