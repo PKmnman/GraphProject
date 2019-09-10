28f61ca7ab8da1a2d93339ebad1ebdc243f99219
PKmnman <gareeves@my.bridgeport.edu>
2019-09-10 17:38:04 -0400

    A lot of big changes! - Implemented dfs - Debugged file loading - Worked on bfs, still not working properly

--- src/com/graph/ListGraph.java
+++ src/com/graph/ListGraph.java
@@ -2,7 +2,7 @@ package com.graph;
 
 import java.util.*;
 
-public class ListGraph<T, V> implements Graph<T, V> {
+public class ListGraph<V> implements Graph<Integer, V> {
 
 
     private ArrayList<ListVertex> vertices;
@@ -12,79 +12,151 @@ public class ListGraph<T, V> implements Graph<T, V> {
     }
 
     public ListGraph(int vertices) {
+        //Initialize the arraylist used to store the vertices
         this.vertices = new ArrayList<>(vertices);
-    }
-
-    public ListGraph(ListVertex... vertices) {
-        this.vertices = new ArrayList<>(vertices.length);
-        Collections.addAll(this.vertices, vertices);
+        for (int i = 0; i < vertices; i++) {
+            //Add default vertices
+            addVertex(i + 1, null);
+        }
+        
     }
 
     @Override
-    public GraphVertex<T, V> depthSearch(GraphVertex vertex, T targetVectorID) {
-
-        List<GraphVertex> vEdges = vertex.getEdges();
-
-        for (int i = 0; i < vEdges.size(); i++) {
-            if (false) {
-                depthSearch(vEdges.get(i), targetVectorID);
-            }
+    public List<? extends GraphVertex> depthSearch(Integer vertexID) {
+        //Check that starting vertex exist
+        int startIndex = indexOfID(vertexID);
+        
+        if(startIndex >= 0){
+            //Get start vertex
+            ListVertex start = vertices.get(startIndex);
+            
+            //Create a list to track the visited nodes
+            List<ListVertex> visited = new LinkedList<>();
+            
+            //Enter recursive part
+            DFS(start, visited);
+            
+            return visited;
         }
-
+        
         return null;
     }
+    
+    private void DFS(ListVertex v, List<ListVertex> visited){
+        
+        visited.add(v);
+    
+        //For each vertex w adjacent to v
+        for(ListVertex w : (List<ListVertex>)(v.getEdges())){
+            //Check if we've visited
+            if(!visited.contains(w)){
+                //Visit
+                //Then dfs(w)
+                DFS(w, visited);
+            }
+        }
+    }
+    
+    /**
+     * Finds the index of the vertex corresponding to the specified
+     * vertexID within this graph's adjacency list.
+     * @param vertexID the id of the vertex to search for, -1 if no matching vertex is found
+     * @return an index
+     */
+    private int indexOfID(int vertexID){
+        for (int i = 0; i < vertices.size(); i++) {
+            if(vertices.get(i).getVectorID().equals(vertexID)){
+                return i;
+            }
+        }
+        
+        return -1;
+    }
 
     @Override
-    public GraphVertex<T, V> breadthSearch(T nodeID) {
+    public List<? extends GraphVertex> breadthSearch(Integer nodeID) {
+        //Check if node even exist
         if (this.contains(nodeID)) {
-            GraphVertex vertex = vertices.get(vertices.indexOf(new ListVertex<T, V>(nodeID, null)));
-            ArrayList<GraphVertex> bfsList = new ArrayList<>();
+            //This is the starting vertex
+            GraphVertex vertex = vertices.get(vertices.indexOf(new ListVertex<V>(nodeID, null)));
+            
+            //The tracking array
+            boolean[] visitedVertices = new boolean[this.vertices.size()];
+            Arrays.fill(visitedVertices, false);
+            LinkedList<ListVertex> result = new LinkedList<>();
+            
+            //The processing queue
             Queue<GraphVertex> visited = new ArrayDeque<>();
-
-            while (visited.peek() != null) {
-                bfsList.add(visited.remove());
+            
+            visited.add(vertex);
+            
+            while (!visited.isEmpty()) {
+                //Pop head of queue into v
+                ListVertex v = (ListVertex)visited.poll();
+                
+                //For each vertex w adjacent to v
                 for (int i = 0; i <vertex.getEdges().size(); i++) {
-                    int[] neighbors = new int[vertex.getEdges().size()];
-                    neighbors[i]= vertex.getEdges().indexOf(i);
-                    for (int j = 0; j < bfsList.size(); j++) {
-                        if (neighbors[i] == bfsList.indexOf(j) ){
-                        }else{
-                            breadthSearch(neighbors[j]);
-                        }
+                    //Init w to edge i
+                    ListVertex w = (ListVertex)vertex.getEdges().get(i);
+                    
+                    //Check if it has not been visited
+                    if(!visitedVertices[vertices.indexOf(w)]){
+                        //Visit
+                        visitedVertices[vertices.indexOf(w)] = true;
+                        result.add(w);
+                        //Queue
+                        visited.add(w);
                     }
                 }
             }
-            return null;
+            
+            return result;
         }
 
-
+        return null;
     }
 
     /**
      * @see com.graph.Graph#addVertex
      **/
     @Override
-    public void addVertex(T vertexID, V value) {
-        ListVertex<T, V> vertex = new ListVertex<>(vertexID, value);
+    public void addVertex(Integer vertexID, V value) {
+        ListVertex<V> vertex = new ListVertex<>(vertexID, value);
 
         if (!vertices.contains(vertex)) {
             vertices.add(vertex);
         }
     }
-
+    
     @Override
-    public List<? extends GraphVertex<T, V>> getVectors() {
-        return new ArrayList<>();
+    public void addVertex(GraphVertex v) {
+        ListVertex vertex = (ListVertex)v;
+    
+        if (!vertices.contains(vertex)) {
+            vertices.add(vertex);
+        }
+        
+    }
+    
+    @Override
+    public List<? extends GraphVertex<Integer, V>> getVertices() {
+        return (List)this.vertices;
     }
 
-    @java.lang.Override
-    public boolean contains(T nodeID) {
-        return false;
+    @Override
+    public boolean contains(Integer nodeID) {
+        return indexOfID(nodeID) != -1;
     }
 
     @Override
     public String toString() {
-        return "A List Graph";
+        StringBuilder sb = new StringBuilder();
+        sb.append("Graph{\n");
+        for (int i = 0; i < vertices.size(); i++) {
+            sb.append(vertices.get(i).toString()).append('\n');
+        }
+        sb.append('}');
+        return sb.toString();
     }
 
 }
--- src/com/graph/ListVertex.java
+++ src/com/graph/ListVertex.java
@@ -5,11 +5,11 @@ import com.graph.Graph.GraphVertex;
 import java.util.ArrayList;
 import java.util.List;
 
-class ListVertex<T, V> extends GraphVertex<T, V> {
+class ListVertex<V> extends GraphVertex<Integer, V> {
     
     private ArrayList<ListVertex> edges = new ArrayList<>(3);
     
-    public ListVertex(T nodeID, V value) {
+    public ListVertex(Integer nodeID, V value) {
         super(nodeID, value);
     }
     
@@ -24,6 +24,13 @@ class ListVertex<T, V> extends GraphVertex<T, V> {
         }
     }
     
+    public void setEdge(ListVertex v, ListVertex w){
+        int i = edges.indexOf(v);
+        if(i >= 0){
+            edges.set(i, w);
+        }
+    }
+    
     @Override
     public boolean removeEdge(GraphVertex vertex) {
         //If
@@ -34,6 +41,37 @@ class ListVertex<T, V> extends GraphVertex<T, V> {
     @Override
     public List<ListVertex> getEdges() {
         return edges;
+    }
+    
+    @Override
+    public boolean equals(Object obj) {
+        if(obj instanceof ListVertex){
+            return ((ListVertex) obj).vectorID.equals(this.vectorID);
+        }
+        return false;
+    }
+    
+    @Override
+    public Integer getVectorID() {
+        return super.getVectorID();
+    }
+    
+    @Override
+    public V getValue() {
+        return super.getValue();
+    }
+    
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        
+        sb.append(this.vectorID);
+        
+        for(ListVertex v : edges){
+            sb.append(">").append(v.getVectorID());
+        }
+        
+        return sb.toString();
     }
     
 }
